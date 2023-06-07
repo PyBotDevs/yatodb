592b4560cdfe3d4def8236c3a26d71623b64f173
snipe <72265661+notsniped@users.noreply.github.com>
2023-06-07 23:17:01 +0530

    Make web server pinger library
    This will be used to ping the replit web-server to prevent the repl from sleeping (which could cause the bot to shut down)

---
+++ utils/ping.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+  return """
+YAToDB is online.
+""", 200
+
+def run():
+    app.run(host="0.0.0.0", port="8080")
+
+def host(*, no_thread: bool = False):
+    if no_thread: run()
+    else:
+        server = Thread(target=run)
+        server.daemon = True
+        server.start()
